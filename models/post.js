d939b064b0e8f081a7d16356c731633e849720da
ThisIsEmma <sakatia.lise@gmail.com>
2023-04-12 22:08:30 -0400

    mid part 4

--- models/post.js
+++ models/post.js
@@ -1,9 +1,12 @@
-const { Schema, model } = require('mongoose');
+//MODEL
+const { Schema, model, mongoose } = require('mongoose');
 
-const postSchema = new Schema({
+// const postSchema = new Schema({
+//   postId: { type: mongoose.SchemaTypes.ObjectId,required: true, index: true },
   title: { type: String, required: true },
   url: { type: String, required: true },
-  summary: { type: String, required: true }
+  summary: { type: String, required: true },
+  // subreddit: { type: String, required: true }
 });
 
 module.exports = model('Post', postSchema);
--- test/posts.js
+++ test/posts.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { describe, it, after } = require('mocha');
+const { after, describe, it } = require('mocha');
 const app = require('../reddit');
 chai.use(chaiHttp);
 const agent = chai.request.agent(app);
@@ -21,38 +21,39 @@ describe('Posts', () => {
   it('should create with valid attributes at POST /posts/new', (done) => {
     // TODO: test code goes here!
     // Checks how many posts there are now
-    Post.estimatedDocumentCount()
-      .then((initialDocCount) => {
-        agent
-          .post('/posts/new')
-          // This line fakes a form post,
-          // since we're not actually filling out a form
-          .set('content-type', 'application/x-www-form-urlencoded')
-          // Make a request to create another
-          .send(newPost)
-          .then((res) => {
-            Post.estimatedDocumentCount()
-              .then((newDocCount) => {
-                // Check that the database has status 200
-                res.should.have.status(200);
-                // Check that the database has one more post in it
-                newDocCount.should.equal(initialDocCount + 1);
-                done();
-              })
-              .catch((err) => {
+        Post.estimatedDocumentCount()
+        .then((initialDocCount) => {
+            agent
+            .post('/posts/new')
+            // This line fakes a form post,
+            // since we're not actually filling out a form
+            .set('content-type', 'application/x-www-form-urlencoded')
+            // Make a request to create another
+            .send(newPost)
+            .then((res) => {
+                Post.estimatedDocumentCount()
+                .then((newDocCount) => {
+                    // Check that the database has status 200
+                    res.should.have.status(200);
+                    // Check that the database has one more post in it
+                    newDocCount.should.equal(initialDocCount + 1);
+                    done();
+                })
+                .catch((err) => {
+                    done(err);
+                });
+            })
+            .catch((err) => {
                 done(err);
-              });
-          })
-          .catch((err) => {
+            });
+        })
+        .catch((err) => {
             done(err);
-          });
-      })
-      .catch((err) => {
-        done(err);
-      });
-  });
-
-  after(() => {
+        });
+        
+    });
+    
+    after(() => {
     Post.findOneAndDelete(newPost);
-  });
+    });
 });
