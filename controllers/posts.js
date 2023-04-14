af99b3dc4c5602ad89c6bb826b511fc868c1370c
ThisIsEmma <sakatia.lise@gmail.com>
2023-04-13 22:34:49 -0400

    everything completed

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,30 +1,54 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
 const Post = require('../models/post');
 
 module.exports = (app) => {
-  // CREATE
-  app.post('/posts/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
-    const post = new Post(req.body);
 
-    // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
-    post.save(() => res.redirect('/'));
-  });
+    //Display all posts in the Homepage
+    app.get('/', (req, res) => {
+        Post.find({}).lean()
+        .then((posts) => res.render('posts-index', { posts }))
+        .catch((err) => {
+            console.log(err.message);
+        })
+    })
 
-  app.get('/', (req, res) => {
-    Post.find({}).lean()
-      .then((posts) => res.render('posts-index', { posts }))
-      .catch((err) => {
-        console.log(err.message);
-      })
-  })
+    // Look up a specific post 
+    app.get('/post-show/:id', (req, res) => {
+        Post.findById(req.params.id).lean()
+        .then((post) => res.render('posts-show', { post }))
+        .catch((err) => {
+            console.log(err.message);
+        });
+    });
 
-  // LOOK UP THE POST
-  app.get('/posts/:id', (req, res) => {
-    Post.findById(req.params.id).lean()
-      .then((post) => res.render('posts-show', { post }))
-      .catch((err) => {
-        console.log(err.message);
-      });
-  });
-  
+    // Look up a specific post 
+    // app.get('/posts/:id', (req, res) => {
+    //     Post.findById(req.params.id).lean()
+    //     .then((post) => res.render('posts-show', { post }))
+    //     .catch((err) => {
+    //         console.log(err.message);
+    //     });
+    // });
+
+    // CREATE
+    app.post('/posts/new', (req, res) => {
+        // INSTANTIATE INSTANCE OF POST MODEL
+        const post = new Post(req.body);
+        
+        // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
+        post.save(() => res.redirect('/'));
+    });
+    
+    // View Subreddit
+    app.get('/n/:subreddit', (req, res) => {
+        Post.find({ subreddit: req.params.subreddit }).lean()
+            .then((posts) => res.render('posts-index', { posts }))
+            .catch((err) => {
+            console.log(err);
+        });
+    });
 };
+
+
--- models/post.js
+++ models/post.js
@@ -2,11 +2,10 @@
 const { Schema, model, mongoose } = require('mongoose');
 
 const postSchema = new Schema({
-  // id: { type: mongoose.SchemaTypes.ObjectId, required: true, index: true },
   title: { type: String, required: true },
   url: { type: String, required: true },
   summary: { type: String, required: true },
-  // subreddit: { type: String, required: true }
+  subreddit: { type: String, required: true }
 });
 
 module.exports = model('Post', postSchema);
